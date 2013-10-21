32f376fc4f248f718c83e39079f118892282df70
Adam Goforth <code@adamgoforth.com>
2013-10-21 18:06:49 +0200

    Use heredoc instead of string concat for HTML
    Instead of concatenating a bunch of strings together to create an HTML
    snippet, use heredoc instead, for clarity.

--- includes/AkismetEdit.class.php
+++ includes/AkismetEdit.class.php
@@ -28,21 +28,26 @@ class AkismetEdit
 {
     public static function createUserJudgeHTML($edit_id, $page_id, $timestamp, $username, $html_diff){
         $article_title = self::getArticleTitleFromID($page_id);
+        $notSpamMsg = wfMsg('not-spam');
 
         // Create the HTML
-        $htmlout  = "<div style=\"border: 1px black solid;\">\n";
-        $htmlout .= "<h4 style=\"background-color: lightgray; padding: 5px;\">" . $article_title . "<br />\n";
-        $htmlout .= $timestamp . "<br />\n";
-        $htmlout .= $username . "</h4>\n";
-        $htmlout .= "<div class=\"diffholder\" style=\"height: 200px; overflow: auto;\">\n";
-        $htmlout .= $html_diff . "\n";
-        $htmlout .= "</div>\n";
-        $htmlout .= "<div style=\"background-color: lightgray\">\n";
-        $htmlout .= "<label for=\"spam-" . $edit_id . "\">\n";
-        $htmlout .= "<input type=\"checkbox\" id=\"spam-" . $edit_id . "\" name=\"not_spam[]\" value=\"" . $edit_id . "\" />\n";
-        $htmlout .= wfMsg('not-spam') . "</label>\n";
-        $htmlout .= "</div>\n";
-        $htmlout .= "</div><br /><br />\n";
+        $htmlout = <<<HTML
+        <div style="border: 1px black solid;">
+            <h4 style="background-color: lightgray; padding: 5px;">
+                $article_title<br />
+                $timestamp<br />
+                $username
+            </h4>
+            <div class="diffholder" style="height: 200px; overflow: auto;">
+                $html_diff
+            </div>
+            <div style="background-color: lightgray">
+                <label for="spam-$edit_id">
+                    <input type="checkbox" id="spam-$edit_id" name="not_spam[]" value="$edit_id" /> $notSpamMsg
+                </label>
+            </div>
+        </div><br /><br />
+HTML;
 
         return $htmlout;
     }
